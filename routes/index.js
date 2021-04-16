df051e2ccf14dfdd2dc5260c1ec0a5a5a417ab44
kanait84 <kanagaraj84@gmail.com>
2021-04-16 08:32:21 +0400

    Udpated files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1/users', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+var db = mongoose.connection;
+
+// handle mongo error
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+  console.log('Database Connected');
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+const testjwt = jwt.sign({
+  exp: Math.floor(Date.now() / 1000) + (60 * 60),
+  data: 'foobar'
+}, 'secret');
+
+console.log('testjwt', testjwt);
+
+jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTg0ODIwNTEsImRhdGEiOiJmb29iYXIiLCJpYXQiOjE2MTg0Nzg0NTF9.4A0UoKpcEuOOb17IkVa-JsLL6dqw5u8xMeYO5s99U-U', 'secret', function(err, decoded) {
+  console.log(decoded);
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,93 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+const db = require('../model/database.js');
+const bcrypt = require('bcryptjs');
+const VerifyToken = require('../model/auth');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//Signup User
+router.post('/SignupUser', function(req, res) {
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    db.User.findOne({email : req.body.email}, (err,result)=>{
+        if (err) return res.status(501).send({auth : false,  message : 'Error on the server.', err});
+        if(!result){
+            db.User.create({
+                    firstname   : req.body.firstname,
+                    lastname    : req.body.lastname,
+                    email       : req.body.email,
+                    password    : hashedPassword,
+                    role        : req.body.role
+                },
+                function (err, user) {
+                    if (err) return res.status(501).send({auth:false, message:"Error on the server.", err})
+                    if (user) return res.status(200).send({ auth: true, message : 'Registered Successfully.'});
+                });
+        }
+        if(result){
+            res.status(409).send({ auth: false, message : 'This email is already exist.' });
+        }
+    });
+});
+
+//LOGIN
+router.post('/login', function(req, res) {
+    db.User.findOne({ email: req.body.email}, function (err, user) {
+        if (err) return res.status(501).send({auth : false,  message : 'Error on the server.', err});
+        if (!user) return res.status(401).send({ auth: false,  mesaage: 'Invalid Email.' });
+        if (user){
+            // check if the password is valid
+            var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            if (!passwordIsValid) return res.status(401).send({ auth: false,  mesaage: 'Invalid Password.'});
+            // create a token
+            var token = jwt.sign({ id: user._id }, db.secret, {
+                expiresIn: 3600 // expires in 1 hour
+            });
+            // return the information including token as JSON
+            res.status(200).send({ auth: true,  message: 'Login Successful' , user:{id:user._id, name:user.firstname,  role:user.role}, token });
+        }
+    });
+});
+
+//updating first or last name
+router.post('/upname/:id', VerifyToken, function(req, res) {
+    if (req.body.field === 'firstname'){
+        db.User.findOneAndUpdate({_id : req.params.id},{
+                firstname   : req.body.name
+            },
+            function (err, data) {
+                if (err) return res.status(501).send({auth:false, message:"Error on the server.", err})
+                if (data) return res.status(200).send({ auth: true, data:data});
+            });
+    }
+    if (req.body.field === 'lastname'){
+        db.User.findOneAndUpdate({_id : req.params.id},{
+                lastname   : req.body.name
+            },
+            function (err, data) {
+                if (err) return res.status(501).send({auth:false, message:"Error on the server.", err})
+                if (data) return res.status(200).send({ auth: true, data:data});
+            });
+    }
+});
+
+//Store Customer Support Tickets
+router.post('/createticket/:id', VerifyToken, function(req, res) {
+    db.supportTicket.create({
+            userId         :   req.params.id,
+            message        :   req.body.message
+        },
+        function (err, data) {
+            if (err) return res.status(501).send({auth:false, message:"Error on the server.", err})
+            if (data) return res.status(200).send({ auth: true, data:data});
+        });
+});
+
+//get list of all Customer Support Tickets
+router.get('/getTickets', VerifyToken, function(req, res, next) {
+    db.supportTicket.find({},(err, data)=>{
+        if (err) return res.status(501).send({auth:false, message:"Error on the server.", err})
+        if (data) return res.status(200).send({ auth: true, data:data });
+    })
 });
 
 module.exports = router;
